10252926e40534a4ddab832bfc06a0b7d297893a
Bishal Neupane <neupanebishal07@gmail.com>
2021-08-29 08:18:34 +0545

    Create post hook and modal

---
+++ web/src/hooks/useCreatePost.ts
@@ -0,0 +1,35 @@
+import firebase from "../utils/initFirebase";
+import { useMutation, useQueryClient } from "react-query";
+
+type TypeCreatPost = {
+  uuid: string;
+  post: string;
+  creatorId: string;
+};
+
+export const useCreatePost = () => {
+  const queryClient = useQueryClient();
+  return useMutation(
+    ({ creatorId, post, uuid }: TypeCreatPost) => {
+      const uid = firebase.auth().currentUser?.uid;
+      return firebase
+        .firestore()
+        .collection("posts")
+        .doc(uuid)
+        .set({ creatorId, post, uuid }, { merge: true });
+    },
+    {
+      onSuccess: (data, variables) => {
+        // queryClient.invalidateQueries("userInfo");
+        console.log(data);
+      },
+    }
+  );
+};
+
+//the posts are going to be on the root level of the firestore database we are going to have
+// userId
+// text or the body of the posts which can have images rich text datastructure
+// the likes as an array of maps
+// the comments will be subcollection inside the posts
+// the comment text and thats it and the ability to edit and remove the comment no reply stuff
--- web/src/pages/dash.tsx
+++ web/src/pages/dash.tsx
@@ -1,19 +1,33 @@
-import { useRouter } from "next/dist/client/router";
 import React from "react";
-import { Layout } from "../components/Layout";
+import { useQuery } from "react-query";
 import { LoggedInLayout } from "../components/LoggedInLayout";
-import { NavBar } from "../components/NavBar";
+import { UserPostModal } from "../components/Modals/UserPostModal";
+import { Spinner } from "../components/Spinner";
 import { useIsAuth } from "../hooks/useIsAuthenticated";
-import firebase from "../utils/initFirebase";
+import { getUserInfo } from "../utils/queryFunctions";
 
 const dash = () => {
   useIsAuth();
-  const router = useRouter();
-  const query = router.query;
+
+  const { data, isLoading } = useQuery("userInfo", getUserInfo);
+  if (isLoading) {
+    return <Spinner size="4" />;
+  }
 
   return (
     <LoggedInLayout>
-      <h1>Hey there</h1>
+      <div className="h-screen">
+        <div className="px-2 py-3 w-full mt-5 shadow-sm border-2 border-gray-200 h-24 bg-white rounded-lg">
+          <div className="flex space-x-2 items-center">
+            <img
+              src={data?.profilePictureUrl}
+              alt={data?.firstName}
+              className="w-16 h-16 rounded-full"
+            />
+            <UserPostModal />
+          </div>
+        </div>
+      </div>
     </LoggedInLayout>
   );
 };
